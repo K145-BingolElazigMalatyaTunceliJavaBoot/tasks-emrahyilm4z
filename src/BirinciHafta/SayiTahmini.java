eedf7555989e248a11e9589439e63934a1775910
emrahyilm4z <emrahyilm4z@gmail.com>
2022-09-20 02:45:42 +0300

    Revert "Birinci hafta ödev tamamlandı"
    This reverts commit 519d72a5b99a71ca99f8f258c90406eececed58c.

--- src/BirinciHafta/SayiTahmini.java
+++ src/BirinciHafta/SayiTahmini.java
@@ -1,25 +1,4 @@
 package BirinciHafta;
 
-import java.util.Random;
-
-/**
- * 0 ile 500 aralığında ve
- * Bu sayılardan 50 ile 100 arasında bir sayı bulunana kadar rastgele sayılar seçilir.
- * 50 ile 100 arasındaki sayının kaçıncı seçimde bulunduğunu bulan program.
- */
-
 public class SayiTahmini {
-    public static void main(String[] args) {
-        Random random = new Random();
-        boolean sayiBul = true;
-        int sayac = 0;
-        while (sayiBul) {
-            int randomSayi = random.nextInt(500);
-            sayac++;
-            if (randomSayi >= 50 && randomSayi < 100) {
-                sayiBul = false;
-            }
-        }
-        System.out.println(sayac);
-    }
 }
