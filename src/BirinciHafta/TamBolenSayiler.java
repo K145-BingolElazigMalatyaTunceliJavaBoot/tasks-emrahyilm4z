519d72a5b99a71ca99f8f258c90406eececed58c
emrahyilm4z <emrahyilm4z@gmail.com>
2022-09-20 01:56:29 +0300

    Birinci hafta ödev tamamlandı

--- src/BirinciHafta/TamBolenSayiler.java
+++
@@ -1,4 +0,0 @@
-package BirinciHafta;
-
-public class TamBolenSayiler {
-}
--- src/BirinciHafta/ToplamAsalSayi.java
+++ src/BirinciHafta/ToplamAsalSayi.java
@@ -1,4 +1,24 @@
 package BirinciHafta;
 
+/**
+ * 3 ile 100 arasında asal sayıların toplamını bulan program.
+ */
+
 public class ToplamAsalSayi {
+    public static void main(String[] args) {
+        int toplamAsalSayi = 0;
+        for (int i = 3; i <= 100; i++) {
+            boolean asalSayiKontrol = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    asalSayiKontrol = false;
+                    break;
+                }
+            }
+            if (asalSayiKontrol) {
+                toplamAsalSayi += i;
+            }
+        }
+        System.out.println(toplamAsalSayi);
+    }
 }
